38626d792f51ef6ab63fdfcbdb356ffd7f208b1f
alysaarmelia <2310817120009@mhs.ulm.ac.id>
2023-12-16 17:04:21 +0800

    Laprak Akhir PemrogramanI

---
+++ PRAK002-2310817120009-AlysaArmelia.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("Selamat Pagi, Alysa Armelia \n");
+    printf("Selamat Siang, Alysa Armelia \n");
+    printf("Selamat Malam, Alysa Armelia");
+}
---
+++ PRAK004-2310817120009-AlysaArmelia.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char nama[30] = "Alysa Armelia";
+    char nim[30]= "2310817120009";
+    
+    printf("##############################\n");
+    printf("#                            #\n");
+    printf("#       %s        #\n", nama);
+    printf("#       %s        #\n", nim);
+    printf("#                            #\n");
+    printf("##############################\n");
+
+    return 0;
+}
