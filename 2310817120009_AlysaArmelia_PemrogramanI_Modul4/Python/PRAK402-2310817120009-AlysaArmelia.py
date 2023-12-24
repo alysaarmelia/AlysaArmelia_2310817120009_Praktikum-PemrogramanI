faace3b54c32ad65dc3ffa91ec524ee74ea53772
Alysa Armelia <145275748+alysaarmelia@users.noreply.github.com>
2023-12-24 22:35:46 +0800

    Update PRAK402-2310817120009-AlysaArmelia.py

--- 2310817120009_AlysaArmelia_PemrogramanI_Modul4/Python/PRAK402-2310817120009-AlysaArmelia.py
+++ 2310817120009_AlysaArmelia_PemrogramanI_Modul4/Python/PRAK402-2310817120009-AlysaArmelia.py
@@ -2,10 +2,10 @@ bilangan = int(input(""))
 
 for non in range(1, bilangan+1):
     if non % 2 == 1:
-        print(non, end='')
+        print(non, end=' ')
 
 print("")
 
 for kok in range (bilangan, 0, -1):
     if kok % 2 == 0:
-        print(kok, end='')
+        print(kok, end=' ')
